a88ecbc9362524319d816f0e81cd34d951731352
SolitaryGreed <padazbze007@gmail.com>
2024-12-23 16:57:34 +0300

    Add new listen and clear global commands

--- src/main/java/mainBot/Main.java
+++ src/main/java/mainBot/Main.java
@@ -1,16 +1,24 @@
 package mainBot;
 
+import commands.Rules;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        final String TOKEN = dotenv.get("TOKEN");
+        final String TOKEN = Dotenv.load().get("TOKEN");
+        final JDA DiscordBot = JDABuilder.createDefault(TOKEN).build();
 
-        JDA jda = JDABuilder.createDefault(TOKEN).build();
+        // Clear global commands
+        DiscordBot.updateCommands().queue();
+
+        DiscordBot.addEventListener(new GuildCommands());
+        DiscordBot.addEventListener(new Listen());
+        DiscordBot.addEventListener(new Rules());
 
     }
+
 }
